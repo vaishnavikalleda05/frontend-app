28aa963c0a13fbf3ec89bbc426bdc4bf9d862fd8
vaishnavikalleda05 <kalledavaishnavi@gmail.com>
2026-03-12 18:10:54 +0530

    updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,38 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import axios from "axios";
-import "./Content.css"
+import "./Content.css";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 function Content() {
   // const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  const { user, setUser, cart, setCart } = useContext(AppContext);
   const fetchProducts = async () => {
-    const url = "https://backend-app-z2t4.onrender.com/store";
+    const url = `${API_URL}/store`;
     const res = await axios.get(url);
     setProducts(res.data);
   };
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const addToCart = (product) => {
+    const found = cart.find((item) => item._id === product._id);
+    if (!found) {
+      product.quantity = 1;
+      setCart([...cart,product]);
+    }
+  };
+
   return (
     <div>
-     
-      {/* <button onClick={decrement}>-</button>
-      {count}
-      <button onClick={increment}>+</button>
-      <hr /> */}
       <div className="row">
         {products.map((product) => (
           <div className="box">
-            <img src={`https://backend-app-z2t4.onrender.com/${product.imageUrl}`} width="300px" alt="" />
+            <img src={`${API_URL}/${product.imageUrl}`} width="300px" alt="" />
             <h3>{product.name}</h3>
             <p>{product.desc}</p>
             <h4>{product.price}</h4>
-            <p><button>Add to Cart</button></p>
+            <p>
+              <button onClick={() => addToCart(product)}>Add to Cart</button>
+            </p>
           </div>
         ))}
       </div>
